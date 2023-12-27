ac303c54b9342360eb7a9cdabca1795a2ec48e34
AD6308 <ad6308@AD6308s-iMac.local>
2023-12-27 15:47:12 +0700

    add new template as iframe

---
+++ wp-content/themes/matichonweekly-theme/page-frame-e-book-register.php
@@ -0,0 +1,15 @@
+<?php
+/* Template Name: frame e-book register*/
+?>
+<style>
+  body{
+    box-sizing: border-box;
+    margin: 0;
+  }
+  iframe{
+    width:100%;
+    height: 100%;
+    margin: 0;
+  }
+</style>
+<iframe src="https://poll.matichon.co.th/bookforgift/" frameborder="0"></iframe>
